6a14cde04d3b18469b422f40394514f83af55bf3
R.Murzakov <romuald.murzakov@gmail.com>
2017-08-07 14:44:55 +0100

    Initial commit

---
+++ app/src/main/java/code_clan/movies/Movies.java
@@ -0,0 +1,11 @@
+package code_clan.movies;
+
+/**
+ * Created by R.Murzakov on 07/08/2017.
+ */
+
+public class Movies {
+    private String movie;
+    private String genre;
+    private int currentRanking;
+}
